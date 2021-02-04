b186961bd58a7385d2e28b7b50b2541b34fb6c1c
drabhishekkumar <draabhishekkumar@gmail.com>
2021-02-04 09:59:25 +0530

    Update list-example9.py

--- List-Python/list-example9.py
+++ List-Python/list-example9.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on TUE 15/12/2020 23:22
-File name: list-example8.py
+File name: list-example9.py
 @author: Abhishek Kumar
 Purpose - Example of Difference in Deletion
 """
